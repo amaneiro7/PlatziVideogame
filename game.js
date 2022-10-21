47281e784618d265baea6bade88ad20f230aabe4
Andres Maneiro <amaneiro7@gmail.com>
2022-10-21 01:26:37 -0400

    Se agrego la logica para el tamaño del canvas y que sea resposive

--- game.js
+++ game.js
@@ -1,9 +1,33 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
 
-window.addEventListener('load', startGame);
+let canvasSize;
+let elementsSize;
 
-function startGame() {
-    game.fillRect(0,0,100,100);
-    game.clearRect(0,0,100,50);
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
+
+
+function setCanvasSize() {
+    if (window.innerHeight > window.innerWidth) {
+        canvasSize = window.innerWidth * 0.8
+    } else {
+        canvasSize = window.innerHeight * 0.8
+    }
+    
+    canvas.setAttribute('width', canvasSize);
+    canvas.setAttribute('height', canvasSize);
+    elementsSize = canvasSize /10;
+    
+    startGame();
+}
+
+function startGame() {   
+      
+    game.font = `${elementsSize}px Verdana` 
+    game.textAlign = 'end'
+
+    for (let i = 1; i <= 10; i++) {
+        game.fillText(emojis['X'], elementsSize * i, elementsSize);
+    }
 }
